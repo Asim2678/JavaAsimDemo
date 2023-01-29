aa21b427952359e28ec3203bcb195db5632ef151
Muhammad Asim Bassra <mabassra@cnsonline.net>
2023-01-29 13:09:28 +0000

    Area.java fill added

---
+++ Area.java
@@ -0,0 +1,15 @@
+public class Area {
+    public static void main (String[] args ) {
+
+        float rdious = 9.6f;
+        final float Pie= 3.14f;
+        double area = Pie *rdious *rdious;
+        System.out.println("Area of circles is :" + area);
+
+        System.out.println("Area of circles is :"+ String.format("%.2f",area));
+
+
+
+    }
+
+}
