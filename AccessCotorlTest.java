10933efc3b0f93013832a8c38c2114a7af0e5310
Asim2678 <asimbassra2002@yahoo.com>
2023-02-05 15:13:49 +0000

    AccessControTest adding to Repo

---
+++ AccessCotorlTest.java
@@ -0,0 +1,18 @@
+public class AccessCotorlTest {
+    public static void main(String[] args) {
+
+        AccessControDemo object1 = new AccessControDemo();
+        object1.a =10;
+        object1.b =20;
+        object1.setC(30);
+        System.out.println(object1.a);
+        System.out.println(object1.b);
+        System.out.println(object1.getc());
+        // where a member of a class i pulblic then it is accessed by another code
+        // if private then it cannot be accessed in other classes and sub classes
+        // can be accessed b only members of its class.
+        // Protected applies only when inheritance is involve do
+
+
+    }
+}
